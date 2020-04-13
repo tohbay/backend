c7d5ad51da4500dde7e585cf909fef814ff42217
tobechukwuobitube <tobechukwuobitube@gmail.com>
2020-04-13 14:53:45 +0100

    update routes

--- src/routes/data.js
+++ src/routes/data.js
@@ -1,9 +1,11 @@
-import { Router } from 'express';
-import DataController from '../controllers/data';
+import { Router } from "express";
+import DataController from "../controllers/data";
 
 const router = Router();
 
-router.post('/', DataController.create);
-router.get('/all', DataController.getAll);
+router.post("/", DataController.create);
+router.post("/json", DataController.create);
+router.post("/xml", DataController.create);
+router.get("/all", DataController.getAll);
 
 export default router;
